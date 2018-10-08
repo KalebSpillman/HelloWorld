a3e4b4e79a118d81a8ebce768c7dbc5e9626119a
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-10-08 10:32:20 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Kaleb Spillman on 10/8/18.
+//  Copyright © 2018 Kaleb Spillman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
